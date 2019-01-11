d95d7432c5e410110be939a4d552220eac197e5e
tripleCC <triplec.linux@gmail.com>
2019-01-11 11:06:16 +0800

    feat: bin search 使用 Source::Aggregate

--- lib/cocoapods-bin/command/bin/search.rb
+++ lib/cocoapods-bin/command/bin/search.rb
@@ -38,7 +38,8 @@ module Pod
 
 	        source = @code ? code_source : binary_source
 
-					sets = source.search_by_name(query_regex)
+	        aggregate = Pod::Source::Aggregate.new([source])
+					sets = aggregate.search_by_name(query_regex, true)
 
 					if(@use_pager)
 	          UI.with_pager { print_sets(sets) }
