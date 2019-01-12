6a6e004820a6879f2781778e13f925c14c1a2f7b
tripleCC <triplec.linux@gmail.com>
2019-01-12 17:35:42 +0800

    fix: ib lint 不会下载 source，所以不能进行二进制 lint, 需要用 spec lint

--- lib/cocoapods-bin/command/bin/lib/lint.rb
+++ lib/cocoapods-bin/command/bin/lib/lint.rb
@@ -15,24 +15,19 @@ module Pod
             CLAide::Argument.new('NAME.podspec', false),
           ]
 
+          # lib lint 不会下载 source，所以不能进行二进制 lint
+          # 要 lint 二进制版本，需要进行 spec lint，此 lint 会去下载 source
           def self.options
             [
-              ['--binary', 'lint 组件的二进制版本'],
-              ['--template-podspec=A.binary-template.podspec', '生成拥有 subspec 的二进制 spec 需要的模版 podspec, 插件会更改 version 和 source'],
               ['--code-dependencies', '使用源码依赖进行 lint'],
-              ['--loose-options', '添加宽松的 options, 可能包括 --use-libraries (可能会造成 entry point (start) undefined)'],
-              ['--reserve-created-spec', '保留生成的二进制 spec 文件'],
+              ['--loose-options', '添加宽松的 options, 包括 --use-libraries (可能会造成 entry point (start) undefined)'],
             ].concat(Pod::Command::Lib::Lint.options).concat(super).uniq
           end
 
           def initialize(argv)
-            @podspec = argv.shift_argument
-            @binary = argv.flag?('binary')
             @loose_options = argv.flag?('loose-options')
             @code_dependencies = argv.flag?('code-dependencies')
             @sources = argv.option('sources') || []
-            @reserve_created_spec = argv.flag?('reserve-created-spec')
-            @template_podspec = argv.option('template-podspec')
             super
 
             @additional_args = argv.remainder!
@@ -44,8 +39,6 @@ module Pod
                 "--sources=#{sources_option(@code_dependencies, @sources)}",
                 *@additional_args
               ]
-
-              argvs << spec_file if spec_file
               
               if @loose_options
                 argvs << '--allow-warnings'
@@ -56,41 +49,9 @@ module Pod
               lint.validate!
               lint.run
             end
-          ensure
-            clear_binary_spec_file_if_needed unless @reserve_created_spec
           end
 
-          private
-
-
-          def template_spec_file
-            @template_spec_file ||= begin
-              if @template_podspec
-                find_spec_file(@template_podspec) 
-              else 
-                binary_template_spec_file
-              end
-            end
-          end
-
-          def spec_file
-            @spec_file ||= begin
-              if @podspec
-                find_spec_file(@podspec) 
-              else
-                raise Informative, "当前目录下没有找到可用源码 podspec." if code_spec_files.empty?
-
-                spec_file = if @binary
-                              code_spec = Pod::Specification.from_file(code_spec_files.first)
-                              template_spec = Pod::Specification.from_file(template_spec_file) if template_spec_file
-                              create_binary_spec_file(code_spec, template_spec)
-                            else
-                              code_spec_files.first
-                            end
-                spec_file
-              end
-            end
-          end
+          
         end
       end
     end
--- lib/cocoapods-bin/command/bin/spec/lint.rb
+++ lib/cocoapods-bin/command/bin/spec/lint.rb
@@ -12,20 +12,27 @@ module Pod
           DESC
 
           self.arguments = [
-            CLAide::Argument.new(%w(NAME.podspec DIRECTORY http://PATH/NAME.podspec), false, true)
+            CLAide::Argument.new(%w(NAME.podspec DIRECTORY http://PATH/NAME.podspec), false, true),
           ]
 
           def self.options
             [
+              ['--binary', 'lint 组件的二进制版本'],
+              ['--template-podspec=A.binary-template.podspec', '生成拥有 subspec 的二进制 spec 需要的模版 podspec, 插件会更改 version 和 source'],
+              ['--reserve-created-spec', '保留生成的二进制 spec 文件'],
               ['--code-dependencies', '使用源码依赖进行 lint'],
-              ['--loose-options', '添加宽松的 options'],
+              ['--loose-options', '添加宽松的 options, 包括 --use-libraries (可能会造成 entry point (start) undefined)'],
             ].concat(Pod::Command::Lib::Spec.options).concat(super).uniq
           end
 
           def initialize(argv)
+            @podspec = argv.shift_argument
             @loose_options = argv.flag?('loose-options')
             @code_dependencies = argv.flag?('code-dependencies')
             @sources = argv.option('sources') || []
+            @binary = argv.flag?('binary')
+            @reserve_created_spec = argv.flag?('reserve-created-spec')
+            @template_podspec = argv.option('template-podspec')
             super
 
             @additional_args = argv.remainder!
@@ -37,12 +44,50 @@ module Pod
                 "--sources=#{sources_option(@code_dependencies, @sources)}",
                 *@additional_args
               ]
-              
-              argvs += ['--allow-warnings', '--use-libraries'] if @loose_options
+
+              argvs << spec_file if spec_file
+
+              if @loose_options
+                argvs += ['--allow-warnings']
+                argvs << '--use-libraries' if code_spec.all_dependencies.any?
+              end
 
               lint = Pod::Command::Spec::Lint.new(CLAide::ARGV.new(argvs))
               lint.validate!
               lint.run
+            end
+          ensure
+            clear_binary_spec_file_if_needed unless @reserve_created_spec
+          end
+
+          private
+
+          def template_spec_file
+            @template_spec_file ||= begin
+              if @template_podspec
+                find_spec_file(@template_podspec) 
+              else 
+                binary_template_spec_file
+              end
+            end
+          end
+
+          def spec_file
+            @spec_file ||= begin
+              if @podspec
+                find_spec_file(@podspec) || @podspec
+              else
+                raise Informative, "当前目录下没有找到可用源码 podspec." if code_spec_files.empty?
+
+                spec_file = if @binary
+                              code_spec = Pod::Specification.from_file(code_spec_files.first)
+                              template_spec = Pod::Specification.from_file(template_spec_file) if template_spec_file
+                              create_binary_spec_file(code_spec, template_spec)
+                            else
+                              code_spec_files.first
+                            end
+                spec_file
+              end
             end
           end
         end
